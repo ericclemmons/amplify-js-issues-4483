a98f493cf1c11c6ef88518529fae9af021405d9a
Eric Clemmons <eric@smarterspam.com>
2019-12-06 14:53:08 -0800

    Add vanilla TextInput

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
-import { Authenticator } from "aws-amplify-react-native";
+import { Authenticator, AmplifyTheme } from "aws-amplify-react-native";
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Text style={AmplifyTheme.inputLabel}>Test</Text>
+      <TextInput style={AmplifyTheme.input}></TextInput>
       <Authenticator amplifyConfig={{}} />
     </View>
   );
